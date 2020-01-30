13b5862919011e47b24dd98b8711ae92b7eb0fc7
7yl4r <git@tylar.info>
2020-01-30 13:16:39 -0500

    + print debugs

--- filter_and_merge_mofs.py
+++ filter_and_merge_mofs.py
@@ -5,20 +5,23 @@ import pandas as pd
 obis_records_file = 'data/FKNMS.csv'
 obis_mofs_file = 'data/FKNMS_mofs.csv'
 obis_records_and_mofs_file = 'data/FKNMS_merged.csv'
+measurement_type = 'fish length'  # the only measurement we care about
 
 # === load
+print('loading dataframes...')
 obis_data = pd.read_csv(obis_records_file, low_memory=False)
 obis_mofs = pd.read_csv(obis_mofs_file, low_memory=False)
 
 # === drop all cols except the few we want to keep
+print('dropping some columns...')
 obis_data = obis_data[[
     'occurrenceID', 'date_year', 'habitat', 'scientificName', 'aphiaID',
     'decimalLatitude', 'decimalLongitude', 'occurrenceStatus',
     'basisOfRecord', 'date_start', 'date_mid', 'date_end',
-    'samplingProtocol', 'recordedBy',
+    'samplingProtocol',
     'maximumDepthInMeters', 'minimumDepthInMeters', 'dataset_id',
-    'eventDate', 'datasetName', 'coordinateUncertaintyInMeters',
-    'ownerInstitutionCode', 'samplingEffort', 'depth', 'individualCount'
+    'eventDate', 'datasetName',
+    'ownerInstitutionCode', 'samplingEffort', 'depth'
 ]]
 obis_mofs = obis_mofs[[
     'measurementValue', 'occurrenceID', 'measurementType'
@@ -26,16 +29,17 @@ obis_mofs = obis_mofs[[
 
 
 # === filter everything except fish_length
-obis_mofs = obis_mofs[obis_mofs['measurementType'] == 'fish length']
-
+print('keeping only "{}" MoFs'.format(measurement_type))
+obis_mofs = obis_mofs[obis_mofs['measurementType'] == measurement_type]
 
 # === join the MoF & occurrence dataframes
-if obis_data.index.name != 'id':
-    # prevents None of ['id'] are in the columns when re-running
-    obis_data = obis_data.set_index('id')
+print("joining dataframes...")
+obis_data = obis_data.set_index('occurrenceID')
 obis_data_merged = obis_mofs.join(
     obis_data, on="occurrenceID", rsuffix="_occur"
 )
 
 # === save
+print("saving output...")
 obis_data_merged.to_csv(obis_records_and_mofs_file)
+print("done")
