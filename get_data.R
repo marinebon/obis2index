db537ad027f580bc1f52c6418203932cbcec6387
7yl4r <git@tylar.info>
2020-01-30 11:55:51 -0500

    reduce size of download

--- get_data.R
+++ get_data.R
@@ -1,14 +1,15 @@
-# setup
-install.packages("devtools")
-devtools::install_github("iobis/robis")
+# TODO: add this setup to DESCRIPTION file
+# install.packages("devtools")
+# install.packages("glue")
+# devtools::install_github("iobis/robis")
 
-install.packages("glue")
 library("glue")
 
 # === variables
 FKNMS_WKT <- "POLYGON((-83.15 25.65, -80.066667 25.65, -80.066667 24.3, -83.15 24.3, -83.15 25.65))"
 START_DATE <- "1994-01-01"  # 1994 is start year of RVC
 END_DATE <- "2020-01-22"  # date of last time the csv files were updated
+MEASUREMENT_TYPE <- 'fish length'
 obis_records_file <- 'data/FKNMS.csv'
 obis_mofs_file <- 'data/FKNMS_mofs.csv'
 
@@ -32,8 +33,17 @@ if (has_cache(obis_records_file)){
         geometry = FKNMS_WKT,
         startdate = as.Date(START_DATE),
         enddate   = as.Date(END_DATE),
-        mof = TRUE
-#         fields    = c('scientificName', 'samplingEffort', 'individualCount', 'occurrenceStatus', 'samplingProtocol', 'mof')
+        mof = TRUE,
+        measurementtype = MEASUREMENT_TYPE,
+        fields    = c(
+            'occurrenceID', 'date_year', 'habitat', 'scientificName', 'aphiaID',
+            'decimalLatitude', 'decimalLongitude', 'occurrenceStatus',
+            'basisOfRecord', 'date_start', 'date_mid', 'date_end',
+            'samplingProtocol', 'recordedBy',
+            'maximumDepthInMeters', 'minimumDepthInMeters', 'dataset_id',
+            'eventDate', 'datasetName', 'coordinateUncertaintyInMeters',
+            'ownerInstitutionCode', 'samplingEffort', 'depth', 'individualCount'
+        )
     )
     # TODO: check for each of the columns we need b/c API doesn't fail on empty
     obis_records_chars <- apply(obis_records,2,as.character)  # convert lists and other non-csv friendly cols
