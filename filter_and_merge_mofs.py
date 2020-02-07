8ad312525b278d3e6a43719a6f244dd2b3eddc6a
7yl4r <git@tylar.info>
2020-02-07 10:54:04 -0500

    make filer&merge a function

--- filter_and_merge_mofs.py
+++ filter_and_merge_mofs.py
@@ -1,48 +1,66 @@
 import pandas as pd
 
 
-# === variables
-obis_records_file = 'data/FKNMS.csv'
-obis_mofs_file = 'data/FKNMS_mofs.csv'
-obis_records_and_mofs_file = 'data/FKNMS_merged.csv'
-measurement_type = 'fish length'  # the only measurement we care about
+def main(measurement_type):
+    """
+        Merges MoF & occurrence dataframes & filters out all MoF columns except
+        the column measurement_type.
 
-# === load
-print('loading dataframes...')
-obis_data = pd.read_csv(obis_records_file, low_memory=False)
-obis_mofs = pd.read_csv(obis_mofs_file, low_memory=False)
+        params:
+        -----------
+        measurement_type : str
+            the only measurement we care about
+            eg: 'fish length'
+    """
+    # === variables
+    obis_records_file = 'data/FKNMS.csv'
+    obis_mofs_file = 'data/FKNMS_fish_len_mofs.csv'
+    obis_records_and_mofs_file = 'data/FKNMS_merged_{}.csv'.format(
+        measurement_type
+    )
 
-# === drop all cols except the few we want to keep
-print('dropping some columns...')
-obis_data = obis_data[[
-    'occurrenceID', 'date_year', 'habitat', 'scientificName', 'aphiaID',
-    'decimalLatitude', 'decimalLongitude', 'occurrenceStatus',
-    'basisOfRecord', 'date_start', 'date_mid', 'date_end',
-    'samplingProtocol',
-    'maximumDepthInMeters', 'minimumDepthInMeters', 'dataset_id',
-    'eventDate', 'datasetName',
-    'ownerInstitutionCode', 'samplingEffort', 'depth'
-]]
-obis_mofs = obis_mofs[[
-    'measurementValue', 'occurrenceID', 'measurementType'
-]]
+    # === load
+    print('loading dataframes...')
+    obis_data = pd.read_csv(obis_records_file, low_memory=False)
+    obis_mofs = pd.read_csv(obis_mofs_file, low_memory=False)
+
+    # === drop all cols except the few we want to keep
+    print('dropping some columns...')
+    obis_data = obis_data[[
+        'occurrenceID', 'date_year', 'habitat', 'scientificName', 'aphiaID',
+        'decimalLatitude', 'decimalLongitude', 'occurrenceStatus',
+        'basisOfRecord', 'date_start', 'date_mid', 'date_end',
+        'samplingProtocol',
+        'maximumDepthInMeters', 'minimumDepthInMeters', 'dataset_id',
+        'eventDate', 'datasetName',
+        'ownerInstitutionCode', 'samplingEffort', 'depth'
+    ]]
+    obis_mofs = obis_mofs[[
+        'measurementValue', 'occurrenceID', 'measurementType'
+    ]]
+
+    # === filter everything except 'measurement_type'
+    print('keeping only "{}" MoFs'.format(measurement_type))
+    obis_mofs = obis_mofs[obis_mofs['measurementType'] == measurement_type]
+    # rename measurementValue column to fish_length
+    obis_mofs = obis_mofs.rename(
+        columns={'measurementValue': measurement_type}
+    )
+    obis_mofs = obis_mofs.drop(columns='measurementType')
+
+    # === join the MoF & occurrence dataframes
+    print("joining dataframes...")
+    obis_data = obis_data.set_index('occurrenceID')
+    obis_data_merged = obis_mofs.join(
+        obis_data, on="occurrenceID", rsuffix="_occur"
+    )
+
+    # === save
+    print("saving output...")
+    obis_data_merged.to_csv(obis_records_and_mofs_file)
+    print("done")
+    return obis_data_merged
 
 
-# === filter everything except fish_length
-print('keeping only "{}" MoFs'.format(measurement_type))
-obis_mofs = obis_mofs[obis_mofs['measurementType'] == measurement_type]
-# rename measurementValue column to fish_length
-obis_mofs = obis_mofs.rename(columns={'measurementValue': 'fish_length'})
-obis_mofs = obis_mofs.drop(columns='measurementType')
-
-# === join the MoF & occurrence dataframes
-print("joining dataframes...")
-obis_data = obis_data.set_index('occurrenceID')
-obis_data_merged = obis_mofs.join(
-    obis_data, on="occurrenceID", rsuffix="_occur"
-)
-
-# === save
-print("saving output...")
-obis_data_merged.to_csv(obis_records_and_mofs_file)
-print("done")
+if __name__ == "__main__":
+    main("fish_length")
