33dbe664b04f99f6c5eee06b9811987d1e9352fd
7yl4r <git@tylar.info>
2020-02-10 11:54:56 -0500

    conform to GoM Status report csv format.
    as described in https://github.com/mandykarnauskas/GoM-Ecosystem-Status-Report/blob/master/plotIndicatorTimeSeries.r

--- obis2index/util/get_species_abundance.py
+++ obis2index/util/get_species_abundance.py
@@ -29,11 +29,27 @@ def get_species_abundance(species_name='Lachnolaimus maximus'):
     # plt.plot(x, y)
     # plt.show()
 
+    # === std csv formatting from
+    # https://github.com/mandykarnauskas/GoM-Ecosystem-Status-Report/blob/master/plotIndicatorTimeSeries.r
+    #   Column 1 is time values, columns 2+ are indicator data
+    #   Time can be in yearly or monthly time steps, MUST be in this format:
+    #      YYYY (e.g., 2011) or mmmYYYY (e.g., Jan1986)
     output_df = pd.DataFrame({
-        "time": x,
-        "count": y
+        "year": x,
+        "abundance": y
     })
+    header = [
+        #   Row 1 is indicator name (main title of plot)
+        {"year": "indicator", "abundance": "abundance"},
+        #   Row 2 is units (y-axis label)
+        {"year": "units", "abundance": "count / sampling-day"},
+        #   Row 3 is spatial extent or other specifying information
+        #    ...?
+        {"year": "extent", "abundance": "FKNMS"},
+    ]
+    output_df = pd.concat([pd.DataFrame(header), output_df], ignore_index=True)
 
+    output_df.set_index('year', inplace=True)
     output_df.to_csv("indicies/abundance_{}.csv".format(
         species_name.replace(' ', '_')
     ))
