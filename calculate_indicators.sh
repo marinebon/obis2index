b420d8791680ee4bf7495f5029a1ca8c8af190c7
7yl4r <git@tylar.info>
2020-02-13 17:57:41 -0500

    partial re-org

--- calculate_indicators.sh
+++ calculate_indicators.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # download OBIS data
-Rscript ./obis2index/get_data.R
+Rscript ./obis2index/indicies/abundance_of_black_grouper/1_get_data.R
 # filter cols & merge MoFs
 python3 ./obis2index/filter_and_merge_mofs.py 'fish length'
 # calculate abundances
-python3 -m obis2index.indicies.get_species_abundances
+python3 -m obis2index.indicies.abundance_of_black_grouper.calculate_indicator
