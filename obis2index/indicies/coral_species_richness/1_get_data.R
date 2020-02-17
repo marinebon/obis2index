8141507521281946f9f9ecf4a3e36b5711e6990d
7yl4r <git@tylar.info>
2020-02-17 12:51:50 -0500

    query for selected corals

--- obis2index/indicies/coral_species_richness/1_get_data.R
+++ obis2index/indicies/coral_species_richness/1_get_data.R
@@ -2,7 +2,23 @@ source('obis2index/util/fetch_data.R')
 
 # === variables
 
-fetch_data(
-    'black_grouper',
-    scientific_name = 'Mycteroperca bonaci'
+fetch_data_multispecies(
+    'corals',
+    scientific_name = c(
+        # NOTE: for this to be a true richness calculation this is assumed
+        #   to be an exhaustive list of all coral species in FKNMS.
+        #   However, this list is not at all close to exhaustive.
+        # hard bottom species
+        'Siderastrea radians',
+        'Porites astreoides',
+        'Favia fragum',
+        'Diploria strigosa',
+        'Dichocoenia stokesii',
+        # patch reef species
+        'Montastraea annularis',
+        'Siderastrea siderea'
+        'Acropora palmata',
+        # bank reef species
+        # Elkhorn, star, and brain corals (already included)
+    )
 )
