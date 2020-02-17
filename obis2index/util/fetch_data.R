3451d1a06644406eae44a5157d1e430e52f6e879
7yl4r <git@tylar.info>
2020-02-17 12:51:36 -0500

    complete (untested) fetch_multispecies

--- obis2index/util/fetch_data.R
+++ obis2index/util/fetch_data.R
@@ -9,6 +9,35 @@ source('obis2index/util/has_cache.R')
 FILEPATH_PREFIX <- "data/FKNMS"
 FKNMS_WKT <- "POLYGON((-83.15 25.65, -80.066667 25.65, -80.066667 24.3, -83.15 24.3, -83.15 25.65))"
 
+fetch_data_multispecies <- function(
+    filepath_id,
+    scientific_names,
+    roi_wkt=FKNMS_WKT,
+    start_date=NULL, end_date=NULL,
+    measurement_type=NULL,
+){
+    obis_records_file <- glue('{FILEPATH_PREFIX}-{filepath_id}-ocr.csv')
+    obis_mofs_file <- glue('{FILEPATH_PREFIX}-{filepath_id}-mof.csv')
+    if (has_cache(obis_records_file) & has_cache(obis_mofs_file)){
+        obis_records <- read.csv(obis_records_file)
+        obis_mofs <- read.csv(obis_mofs_file)
+    }else{
+        for(species in scientific_names){
+            this_obis_records, this_obis_mofs = fetch_data(
+                species, roi_wkt, start_date, end_date, measurement_type
+            )
+            obis_records <- rbind(obis_records, this_obis_records)
+            obis_mofs <- rbind(obis_mofs, this_obis_mofs)
+        }
+        # convert lists and other non-csv friendly cols
+        obis_records_chars <- apply(obis_records, 2, as.character)
+        write.csv(obis_records_chars, file = obis_records_file)
+        write.csv(obis_mofs, file = obis_mofs_file)
+    }
+    return(obis_records, obis_mofs)
+}
+
+
 fetch_data <- function(
     filepath_id,
     roi_wkt=FKNMS_WKT,
@@ -53,4 +82,5 @@ fetch_data <- function(
         )
         write.csv(obis_mofs, file = obis_mofs_file)
     }
+    return(obis_records, obis_mofs)
 }
