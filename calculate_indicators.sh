cc2b1fff1363f30180c2390b4006d39cde9d6cd8
7yl4r <git@tylar.info>
2020-02-17 11:57:28 -0500

    update main script

--- calculate_indicators.sh
+++ calculate_indicators.sh
@@ -2,6 +2,9 @@
 set -e
 # download OBIS data
 Rscript ./obis2index/indicies/abundance_of_black_grouper/1_get_data.R
+Rscript ./obis2index/indicies/abundance_of_hogfish/1_get_data.R
+Rscript ./obis2index/indicies/abundance_of_red_grouper/1_get_data.R
+Rscript ./obis2index/indicies/abundance_of_yellowtail_snapper/1_get_data.R
 # # calculate abundances
 python3 -m obis2index.indicies.abundance_of_black_grouper.calculate_indicator
 python3 -m obis2index.indicies.abundance_of_hogfish.calculate_indicator
