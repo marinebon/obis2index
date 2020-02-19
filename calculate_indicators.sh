17c1bccf10ef40883d9254c4b7c20a471ae383d7
7yl4r <git@tylar.info>
2020-02-19 15:07:44 -0500

    re-arrange main script lines

--- calculate_indicators.sh
+++ calculate_indicators.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e
-# download OBIS data
+# === abundance
 Rscript ./obis2index/indicies/abundance_of_black_grouper/1_get_data.R
-Rscript ./obis2index/indicies/abundance_of_hogfish/1_get_data.R
-Rscript ./obis2index/indicies/abundance_of_red_grouper/1_get_data.R
-Rscript ./obis2index/indicies/abundance_of_yellowtail_snapper/1_get_data.R
-Rscript ./obis2index/indicies/coral_species_richness/1_get_data.R
-# # calculate abundances
 python3 -m obis2index.indicies.abundance_of_black_grouper.calculate_indicator
+
+Rscript ./obis2index/indicies/abundance_of_hogfish/1_get_data.R
 python3 -m obis2index.indicies.abundance_of_hogfish.calculate_indicator
+
+Rscript ./obis2index/indicies/abundance_of_red_grouper/1_get_data.R
 python3 -m obis2index.indicies.abundance_of_red_grouper.calculate_indicator
+
+Rscript ./obis2index/indicies/abundance_of_yellowtail_snapper/1_get_data.R
 python3 -m obis2index.indicies.abundance_of_yellowtail_snapper.calculate_indicator
+
+# === richness
+Rscript ./obis2index/indicies/coral_species_richness/1_get_data.R
 python3 -m obis2index.indicies.coral_species_richness.calculate_indicator
